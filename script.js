a03429af294aec05a17dd6fbe9e3b651136a750d
Ali <89438079+AliBa1@users.noreply.github.com>
2023-12-10 23:21:56 -0800

    Works to hover changes color

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-gridDiv = document.querySelector(".grid");
+let gridDiv = document.querySelector(".grid");
+let button = document.querySelector("button");
 
-// make boxes for the grid inside of the gridDiv
+let squares = 16;
+let boxWidth = (1/squares) * 100;
+boxWidth = boxWidth + '%';
 
-// gridDiv.appendChild();
+// Add boxes initally
+for (let i = 0; i<squares * squares; i++) {
+    const box = document.createElement('div');
+    box.className = "box";
+    box.style.width = boxWidth;
+    gridDiv.appendChild(box);
+
+    box.addEventListener ("mouseover" , function() {
+        box.style.backgroundColor = "black";
+    })
+}
+
+// when clicked fill with correct number of boxes
+button.addEventListener ("click", function() {
+    // remove boxes
+    for (let i = 0; i<squares * squares; i++) {
+        const box = document.querySelector('.box');
+        box.remove();
+    }
+
+    // take prompt and give values
+    squares = prompt("How many squares do you want on each side?");
+    boxWidth = (1/squares) * 100;
+    boxWidth = boxWidth + '%';
+    let boxClass = document.querySelector(".box");
+    boxClass.style.width = boxWidth;
+    if (squares>100) {
+        squares = 100;
+    } else if (squares < 2) {
+        squares = 2;
+    }
+    console.log(squares);
+    console.log(boxWidth);
+
+    // make boxes
+    for (let i = 0; i<squares * squares; i++) {
+        const box = document.createElement('div');
+        box.className = "box";
+        box.style.width = boxWidth;
+        gridDiv.appendChild(box);
+    
+        box.addEventListener ("mouseover" , function() {
+            box.style.backgroundColor = "black";
+        })
+    }
+})
+
+
+
+
