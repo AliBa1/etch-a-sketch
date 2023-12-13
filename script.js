44c31caa0199aec8ae01dbce050ec21b64da7bee
Ali <89438079+AliBa1@users.noreply.github.com>
2023-12-12 21:41:20 -0800

    Complete (V1)

--- script.js
+++ script.js
@@ -2,56 +2,53 @@ let gridDiv = document.querySelector(".grid");
 let button = document.querySelector("button");
 
 let squares = 16;
-let boxWidth = (1/squares) * 100;
-boxWidth = boxWidth + '%';
 
-// Add boxes initally
-for (let i = 0; i<squares * squares; i++) {
-    const box = document.createElement('div');
-    box.className = "box";
-    box.style.width = boxWidth;
-    gridDiv.appendChild(box);
-
-    box.addEventListener ("mouseover" , function() {
-        box.style.backgroundColor = "black";
-    })
-}
+addBoxes(squares);
 
 // when clicked fill with correct number of boxes
 button.addEventListener ("click", function() {
-    // remove boxes
-    for (let i = 0; i<squares * squares; i++) {
-        const box = document.querySelector('.box');
-        box.remove();
-    }
-
     // take prompt and give values
     squares = prompt("How many squares do you want on each side?");
-    boxWidth = (1/squares) * 100;
-    boxWidth = boxWidth + '%';
-    let boxClass = document.querySelector(".box");
-    boxClass.style.width = boxWidth;
+
     if (squares>100) {
         squares = 100;
     } else if (squares < 2) {
         squares = 2;
     }
+
+    // let boxClass = document.querySelector(".box");
+    // boxClass.style.width = setBoxWidth(squares);
     console.log(squares);
-    console.log(boxWidth);
+    console.log(setBoxSize(squares));
+
+    // remove boxes
+    while (gridDiv.firstChild) {
+        gridDiv.removeChild(gridDiv.firstChild);
+    }
 
     // make boxes
+    addBoxes(squares);
+})
+
+function addBoxes(squares){
+    // Add boxes initally
     for (let i = 0; i<squares * squares; i++) {
         const box = document.createElement('div');
         box.className = "box";
-        box.style.width = boxWidth;
+        box.style.width = setBoxSize(squares);
+        box.style.height = setBoxSize(squares);
         gridDiv.appendChild(box);
-    
+
         box.addEventListener ("mouseover" , function() {
             box.style.backgroundColor = "black";
         })
     }
-})
+}
 
-
+function setBoxSize(squares){
+    let boxWidth = (1/squares) * 100;
+    boxWidth = boxWidth + '%';
+    return boxWidth;
+}
 
 
